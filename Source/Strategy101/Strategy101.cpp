a6e7faf92fb970ba959e624e72c0a9f2bb174a80
Sabrina Lin Shu Wen <lin04sab@gmail.com>
2026-02-27 13:27:12 +0100

    Day 1: UE5.6 C++ project setup + folder structure

---
+++ Source/Strategy101/Strategy101.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Strategy101.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Strategy101, "Strategy101" );
